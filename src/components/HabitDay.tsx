8e4504e61c5f4d2d0645dd0a09948b1bcebd9f85
Gui-dev <adrianon10@gmail.com>
2023-01-30 04:19:28 -0300

    style: add background color

---
+++ src/components/HabitDay.tsx
@@ -0,0 +1,42 @@
+import * as Popover from '@radix-ui/react-popover'
+import clsx from 'clsx'
+
+import { Progressbar } from './Progressbar'
+
+interface HabitDayProps {
+  completed: number
+  amount: number
+}
+
+export const HabitDay = ({ completed, amount }: HabitDayProps) => {
+  const completedPercentage = Math.round((completed / amount) * 100)
+
+  return (
+    <Popover.Root>
+      <Popover.Trigger
+        className={
+          clsx('h-10 w-10 border rounded',
+            {
+              'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+              'bg-violet-900 border-violet-700': completedPercentage >= 0 && completedPercentage < 20,
+              'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
+              'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+              'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+              'bg-violet-500 border-violet-400': completedPercentage >= 80
+            }
+          )
+        }
+      />
+      <Popover.Portal>
+        <Popover.Content
+          className="flex flex-col p-6 min-w-[320px] bg-zinc-900"
+        >
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+          <span className="font-semibold text-zinc-400">sábado</span>
+          <span className="font-extrabold text-3xl leading-tight">28/01</span>
+          <Progressbar progress={completedPercentage} />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  )
+}
