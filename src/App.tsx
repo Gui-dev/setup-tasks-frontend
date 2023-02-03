c02d369488cab56d274859e09a846ceb69e3149e
Gui-dev <adrianon10@gmail.com>
2023-02-03 02:07:48 -0300

    feat: add component ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import './lib/dayjs'
+import 'react-toastify/dist/ReactToastify.min.css'
+
+import { ToastContainer } from 'react-toastify'
+
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
 
@@ -10,6 +14,7 @@ function App () {
       <div className="flex flex-col gap-16 px-6 w-full max-w-5xl">
         <Header />
         <SummaryTable />
+        <ToastContainer position="top-center" theme="dark" />
       </div>
     </section>
   )
