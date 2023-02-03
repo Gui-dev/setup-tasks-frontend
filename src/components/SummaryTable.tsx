1830ba9007171988a2e84dec5f520cbc87ca1cf2
Gui-dev <adrianon10@gmail.com>
2023-02-03 02:11:47 -0300

    feat: add fetch all summaries

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,11 +1,30 @@
+import dayjs from 'dayjs'
+import { useEffect, useState } from 'react'
+import { api } from '../lib/api'
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 import { HabitDay } from './HabitDay'
 
+type Summary = Array<{
+  id: string
+  date: string
+  amount: number
+  completed: number
+}>
+
 export const SummaryTable = () => {
+  const [summary, setSummary] = useState<Summary>([])
   const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
   const summaryDates = generateDatesFromYearBeginning()
   const minimumSummaryDatesSize = 18 * 7 // 18 weeks
   const amountOdDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
+  useEffect(() => {
+    const loadSummary = async () => {
+      const { data } = await api.get('/summary')
+      setSummary(data.summaries)
+    }
+    loadSummary()
+  }, [])
 
   return (
     <div className="flex w-full mb-10">
@@ -23,11 +42,15 @@ export const SummaryTable = () => {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(summaryDate => {
+          const dayInSummary = summary.find(day => {
+            return dayjs(summaryDate).isSame(day.date, 'day')
+          })
           return (
             <HabitDay
               key={summaryDate.toString()}
-              completed={Math.round(Math.random() * 5)}
-              amount={5}
+              date={summaryDate}
+              completed={dayInSummary?.completed}
+              amount={dayInSummary?.amount}
             />
           )
         })}
